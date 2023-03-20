8ef8b32efef778a567314e720a146bcdc61a972f
bravexample <laiyongdian@gmail.com>
2023-03-20 14:29:13 -0400

    a

---
+++ 40742205S_HW1/README.md
@@ -0,0 +1,71 @@
+# Requirement
+The content following is written in chinese,  
+if there are errors displaying the characters,  
+please install noto-fonts-cjk or other chinese fonts you prefer.  
+  
+# 個人資料
+- 系級：化學系
+- 學號：40742205S
+- 姓名：賴勇典
+  
+# 前提
+需要提供標頭檔的作業，包括 potato ball ，**我都直接將函式內容寫進標頭檔，沒有額外的來源檔，**  
+題目敘述裡沒有要求，我想應該是沒問題的。  
+再來，為避免嫌疑，我寫的程式沒有使用 string.h ，僅在 hw0104.c 中使用 mystring.h 。  
+關於 oop.h ，**我保證沒有修改結構體與函式宣告**，僅修改＼增加以下內容：  
+- 將 #pragma once 改成 #ifndef/#endif
+- include stdio.h
+- include stdlib.h
+- include time.h
+- 把 enum MagicalEffect 從單行變成多行
+- 實作函式內容
+  
+**以下為各檔案的介紹。**  
+  
+# mystring.h
+- mystrchr、mystrrchr：  
+傳入空指標會直接回傳 0 ，'\0' 也被視為字串內容。
+- mystrspn、mystrstr：  
+只要其中一個指標為 NULL 就會回傳 0 。
+- mystrcspn：  
+const char *s 若為 NULL 就會回傳 0 ，  
+const char *reject 若為 NULL 會執行 reject = ""; 。
+- mystrpbrk：  
+const char *s 若為 NULL 就會回傳 0 ，  
+const char *accept 若為 NULL 會回傳 (char *)s; 。
+- mystrtok：  
+const char *delim 若為 NULL 就會回傳 0 ，  
+使用 char *mystrtok_ptr 和 char *mystrtok_save 儲存資料，  
+**請不要重複宣告或使用他們**
+  
+# insert.h
+char **ppResult 為 NULL 會回傳 -1 ，  
+const char *pStr1 與 const char *pStr2 皆為 NULL 時會回傳 -1 ，  
+int32_t location 大於 pStr1 的長度時也會回傳 -1 。  
+若 pStr1 或 pStr2 之一為 0 會指派 "" 。
+  
+# abacus.h
+- abacus_init：  
+使用 calloc 。
+- abacus_set：  
+傳入空指標或字串長度超過 255 就會回傳 -1 。
+- abacus_add：  
+傳入空指標會回傳 -1 ，  
+溢位時會進位，若超過 255 也會回傳 -1 。
+- abacus_del：  
+不會改變 sAbacus.number ，  
+如 123 - 123 ， sAbacus.number = 3 ，儲存的值為 0 。  
+若結果為負數會回傳 -1 。
+- abacus_print：  
+不會印出開頭的 0 ，若 pUpperRod 與 pLowerRod 皆為 0 則會印出 0 。
+  
+# hw0104.c
+沒什麼好說的，就 JSON Reader 。
+  
+# oop.h
+我沒有做任何錯誤處理，傳入空指標保證 segmentation fault 。  
+**可以的話請不要做這種事。**
+  
+
+
+**檔案至此結束，感謝您的閱讀。**
---
+++ 40742205S_HW1/mystring.h
@@ -0,0 +1,146 @@
+#include <stddef.h>
+
+char *mystrchr(const char *s, int c) {
+    if (!s)
+        return NULL;
+
+    for (; *s; s++)
+        if (*s == c)
+            return (char *)s;
+    
+    if (!c)
+        return (char *)s;
+    
+    return NULL;
+}
+
+char *mystrrchr(const char *s, int c) {
+    if (!s)
+        return NULL;
+
+    const char *temp = NULL;
+    for (; *s; s++)
+        if (*s == c)
+            temp = s;
+    
+    if (!c)
+        return (char *)s;
+    
+    if (temp)
+        return (char *)temp;
+    
+    return NULL;
+}
+
+size_t mystrspn(const char *s, const char *accept) {
+    if (!s || !accept)
+        return 0;
+
+    size_t count = 0, i = 0;
+    for (; *s; s++, count++) {
+        for (i = 0; accept[i]; i++)
+            if (*s == accept[i])
+                break;
+        
+        if (!accept[i])
+            return count;
+    }
+
+    return count;
+}
+
+size_t mystrcspn(const char *s, const char *reject) {
+    if (!s)
+        return 0;
+
+    if (!reject)
+        reject = "";
+
+    size_t count = 0, i = 0;
+    for (; *s; s++, count++) {
+        for (i = 0; reject[i]; i++)
+            if (*s == reject[i])
+                break;
+        
+        if (reject[i])
+            return count;
+    }
+
+    return count;
+}
+
+char *mystrpbrk(const char *s, const char *accept) {
+    if (!s)
+        return NULL;
+    
+    if (!accept)
+        return (char *)s;
+
+    for (; *s; s++)
+        for (size_t i = 0; accept[i]; i++)
+            if (*s == accept[i])
+                return (char *)s;
+    
+    return NULL;
+}
+
+char *mystrstr(const char *haystack, const char *needle) {
+    if  (!haystack || !needle)
+        return NULL;
+
+    size_t i = 0;
+    for (; *haystack; haystack++)
+        if (*haystack == *needle) {
+            for (i = 0; needle[i]; i++)
+                if (haystack[i] != needle[i])
+                    break;
+            
+            if (!needle[i])
+                return (char *)haystack;
+        }
+
+    return NULL;
+}
+
+
+char *mystrtok_ptr  = NULL;
+char *mystrtok_save = NULL;
+char *mystrtok(char *str, const char *delim) {
+    if (!delim)
+        delim = "";
+
+    if (!str) {
+        if (!mystrtok_save)
+            return NULL;
+        
+        mystrtok_ptr = mystrtok_save;
+    } else
+        mystrtok_ptr = str;
+
+    size_t i = 0;
+    for (; *mystrtok_ptr; mystrtok_ptr++) {
+        for (i = 0; delim[i]; i++)
+            if (*mystrtok_ptr == delim[i])
+                break;
+        
+        if (!delim[i])
+            break;
+    }
+
+    if (!*mystrtok_ptr) {
+        mystrtok_save = NULL;
+        return NULL;
+    }
+
+    size_t j = 0;
+    for (i = 0; mystrtok_ptr[i]; i++)
+        for (j = 0; delim[j]; j++)
+            if (mystrtok_ptr[i] == delim[j]) {
+                mystrtok_ptr[i] = '\0';
+                mystrtok_save = *(mystrtok_ptr + i + 1) ? mystrtok_ptr + i + 1 : NULL;
+                return mystrtok_ptr;
+            }
+
+    mystrtok_save = NULL;
+    return mystrtok_ptr;
+}
