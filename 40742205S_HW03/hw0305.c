1193cf14091121022da56f7431be929d67ceffe6
bravexample <laiyongdian@gmail.com>
2023-05-16 02:41:36 -0400

    a

---
+++ 40742205S_HW03/hw0305.c
@@ -0,0 +1,123 @@
+// A program to turn a txt file to MIDI file
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include "midi.h"
+
+// Max path length
+#define MAX_PATH_LEN 4096
+
+// Main
+int32_t main(void) {
+    // Display the menu
+    printf("Welcome to Cool Sound Infinity Exporter!\n" \
+           "Please enter input and output file name.\n" \
+           "Input file name: ");
+    
+    // Get the path of file
+    uint8_t input_file[MAX_PATH_LEN];
+    if (!fgets(input_file, MAX_PATH_LEN, stdin)) {
+        perror("fgets");
+        return 1;
+    }
+    // Remove the newline character
+    input_file[strlen(input_file) - 1] = '\0';
+
+    // Get the output file name
+    printf("Output file name: ");
+    uint8_t output_file[MAX_PATH_LEN];
+    if (!fgets(output_file, MAX_PATH_LEN, stdin)) {
+        perror("fgets");
+        return 1;
+    }
+    // Remove the newline character
+    output_file[strlen(output_file) - 1] = '\0';
+
+    // Open the input file with mmap
+    int32_t input_fd = open(input_file, O_RDONLY);
+    if (input_fd < 0) {
+        perror("open");
+        return 1;
+    }
+    struct stat input_stat;
+    if (fstat(input_fd, &input_stat) < 0) {
+        perror("fstat");
+        return 1;
+    }
+    uint8_t *input_mmap = mmap(NULL, input_stat.st_size, PROT_READ, MAP_PRIVATE, input_fd, 0);
+    if (input_mmap == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    // Open the output file
+    FILE *output_fp = fopen(output_file, "wb");
+    if (!output_fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    // Create the header chunk
+    sMIDIHeader header_chunk;
+    header_chunk.chunk_id[0] = 'M';
+    header_chunk.chunk_id[1] = 'T';
+    header_chunk.chunk_id[2] = 'h';
+    header_chunk.chunk_id[3] = 'd';
+    header_chunk.chunk_size = 6;
+    header_chunk.format_type = 1;
+
+    // Get the number of tracks
+    header_chunk.track_count = 0;
+    for (uint32_t i = 0; i < input_stat.st_size; i++) {
+        if (input_mmap[i] == '\n')
+            header_chunk.track_count++;
+    }
+
+    // Set the time division
+    header_chunk.time_division = 480;
+
+    // Write the header chunk use big endian
+    {
+        // Write the chunk id
+        fwrite(header_chunk.chunk_id, sizeof(uint8_t), 4, output_fp);
+        // Write the chunk size
+        uint8_t chunk_size[4];
+        chunk_size[0] = (header_chunk.chunk_size >> 24) & 0xFF;
+        chunk_size[1] = (header_chunk.chunk_size >> 16) & 0xFF;
+        chunk_size[2] = (header_chunk.chunk_size >> 8) & 0xFF;
+        chunk_size[3] = header_chunk.chunk_size & 0xFF;
+        fwrite(chunk_size, sizeof(uint8_t), 4, output_fp);
+        // Write the format type
+        uint8_t format_type[2];
+        format_type[0] = (header_chunk.format_type >> 8) & 0xFF;
+        format_type[1] = header_chunk.format_type & 0xFF;
+        fwrite(format_type, sizeof(uint8_t), 2, output_fp);
+        // Write the track count
+        uint8_t track_count[2];
+        track_count[0] = (header_chunk.track_count >> 8) & 0xFF;
+        track_count[1] = header_chunk.track_count & 0xFF;
+        fwrite(track_count, sizeof(uint8_t), 2, output_fp);
+        // Write the time division
+        uint8_t time_division[2];
+        time_division[0] = (header_chunk.time_division >> 8) & 0xFF;
+        time_division[1] = header_chunk.time_division & 0xFF;
+        fwrite(time_division, sizeof(uint8_t), 2, output_fp);
+    }
+
+    // Write the track chunk
+    uint8_t track_chunk_id[4] = {'M', 'T', 'r', 'k'};
+    // Loop for each track
+    while (1) {
+        int32_t track_size = 0;
+        
+    }
+
+    // Exit
+    return 0;
+}
